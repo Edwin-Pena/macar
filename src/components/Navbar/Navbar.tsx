0d9f6f6d7da74f349e995af10fa8705c881505d1
Edwin Peña <edwinsp095@gmail.com>
2025-03-11 17:05:36 -0500

    Added functionality and styles to make the navbar background white when scrolling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,27 @@
+import { useEffect, useState } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="nav">
-      <a href="" className="nav-option">
+    <nav className={`nav ${scrolled ? "scrolled" : ""}`}>
+      <a href="#" className="nav-option">
         Nosotros
       </a>
-      <a href="" className="nav-logo">
-        Macár
+      <a href="#" className="nav-logo">
+        macár
       </a>
     </nav>
   );
