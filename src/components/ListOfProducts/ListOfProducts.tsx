62aa1b09ef8c814a7232a41f0305955bfe80f47d
Edwin Peña <edwinsp095@gmail.com>
2025-03-11 16:58:55 -0500

    Added a function to identify the product that is clicked and display its corresponding page

--- src/components/ListOfProducts/ListOfProducts.tsx
+++ src/components/ListOfProducts/ListOfProducts.tsx
@@ -3,19 +3,32 @@ import "./ListOfProducts.css";
 interface InfoProducts {
   id: string;
   title: string;
-  img: string;
+  images: string[];
+  price: string;
 }
 
 interface Props {
   products: InfoProducts[];
+  onProductClick: (id: string) => void;
 }
 
-const ListOfProducts: React.FC<Props> = ({ products }) => {
+const ListOfProducts: React.FC<Props> = ({ products, onProductClick }) => {
   return (
     <div className="content">
       {products.map((product) => (
-        <div className="product-container" key={product.id}>
-          <img src={product.img} alt={product.title} className="product" />
+        <div
+          className="product-container"
+          key={product.id}
+          onClick={() => {
+            onProductClick(product.id);
+          }}
+        >
+          <img
+            src={product.images[0]}
+            alt={product.title}
+            className="product"
+            loading="lazy"
+          />
           <span className="product-title">{product.title}</span>
         </div>
       ))}
