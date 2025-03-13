facc93ab682fdf586a9ed546250c156f3b654354
Edwin Peña <edwinsp095@gmail.com>
2025-03-13 13:01:59 -0500

    Changing the size and styles of the back-to-home button

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -98,9 +98,11 @@ const ProductPage: React.FC<Props> = ({ product, onBack }) => {
         <div className="back-button" onClick={onBack}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="14"
-            height="14"
-            fill="currentColor"
+            width="12"
+            height="12"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1"
             className="bi bi-chevron-left chevron-back"
             viewBox="0 0 16 16"
           >
