0f5cf9cdf48edb94c38f4c251011987c12ab989d
Edwin Peña <edwinsp095@gmail.com>
2025-03-13 21:40:16 -0500

    Adding images for About US

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -5,6 +5,9 @@ interface AboutUsProps {
 }
 
 const AboutUs: React.FC<AboutUsProps> = ({ onNavigate }) => {
+  const aboutBannerImg = "/images/about-banner.png";
+  const sustainabilityImg = "/images/sustainability.jpg";
+  const detailsImg = "/images/details.jpg";
   return (
     <div className="about-container">
       <div className="back-button" onClick={() => onNavigate("home")}>
@@ -23,67 +26,86 @@ const AboutUs: React.FC<AboutUsProps> = ({ onNavigate }) => {
       </div>
 
       <div className="about-content">
-        <div className="image-container">
-          <div className="principal-img"></div>
-          <h2 className="about-title">MÁS QUE UNA MARCA</h2>
+        <img
+          src={aboutBannerImg}
+          className="principal-img"
+          alt="Mi abuela"
+        ></img>
+        <div className="about-text-container">
+          <h2 className="title">MÁS QUE UNA MARCA</h2>
+          <p className="about-text section-padding">
+            Es la extensión de mi esencia. Soy una mujer soñadora, apasionada y
+            creativa que encuentra en la moda un mundo muy interesante. Me
+            fascina el olor de las telas, el sentir de las texturas y el sonido
+            de la aguja al coser. macár es el reflejo de las mujeres que han
+            marcado mi vida. Encuentro inspiración en su valentía, su fuerza y
+            el amor con la que enfrentan cada día. Es a través de ellas que he
+            construido este universo, un espacio donde cada prenda es más que
+            ropa: es una forma de expresión, un aliado para sentirte segura y
+            auténtica. Aquí descubrirás mi visión de la moda, mis sentimientos,
+            mis pensamientos y mi proceso.
+          </p>
+          <p className="about-text section-padding">
+            Cada diseño nace con la intención de eliminar frustraciones, de
+            abrazar la diversidad de cuerpos y de ofrecer piezas con las que
+            puedas sentirte cómoda, poderosa y libre. Al igual que tú, soy una
+            mujer con un gran sueño. Estoy construyéndolo, y quiero que me
+            acompañes en este camino. Juntas, hagamos de macár un espacio donde
+            la moda nos empodere, donde cada detalle cuente y donde nada nos
+            detenga.
+          </p>
         </div>
-        <p className="about-text section-padding">
-          Es la extensión de mi esencia. Soy una mujer soñadora, apasionada y
-          creativa que encuentra en la moda un mundo muy interesante. Me fascina
-          el olor de las telas, el sentir de las texturas y el sonido de la
-          aguja al coser. macár es el reflejo de las mujeres que han marcado mi
-          vida. Encuentro inspiración en su valentía, su fuerza y el amor con la
-          que enfrentan cada día. Es a través de ellas que he construido este
-          universo, un espacio donde cada prenda es más que ropa: es una forma
-          de expresión, un aliado para sentirte segura y auténtica. Aquí
-          descubrirás mi visión de la moda, mis sentimientos, mis pensamientos y
-          mi proceso.
-        </p>
-        <p className="about-text section-padding">
-          Cada diseño nace con la intención de eliminar frustraciones, de
-          abrazar la diversidad de cuerpos y de ofrecer piezas con las que
-          puedas sentirte cómoda, poderosa y libre. Al igual que tú, soy una
-          mujer con un gran sueño. Estoy construyéndolo, y quiero que me
-          acompañes en este camino. Juntas, hagamos de macár un espacio donde la
-          moda nos empodere, donde cada detalle cuente y donde nada nos detenga.
-        </p>
       </div>
 
-      <div className="about-content section-padding">
+      <div className="about-content">
         <div className="image-container">
-          <div className="about-img"></div>
+          <img
+            src={sustainabilityImg}
+            className="about-img"
+            alt="Tela macár malla"
+          ></img>
           <div className="about-title">
-            <h2 className="title">SOSTENIBILIDAD</h2>
-            <span className="subtitle">FASE 1</span>
+            <h2 className="title white-text">SOSTENIBILIDAD</h2>
+            <span className="subtitle white-text">FASE 1</span>
           </div>
         </div>
-        <p className="about-text">
-          Para nosotros, la sostenibilidad y la calidad son pilares
-          fundamentales en nuestro universo. Creemos en una moda consciente y
-          responsable, y aunque estamos en el inicio de este camino, cada paso
-          cuenta.
-        </p>
-        <p className="about-text">
-          En esta primera fase, hemos dado el primer paso con empaques
-          elaborados con materiales biodegradables, contribuyendo así a reducir
-          nuestro impacto ambiental. Este es solo el comienzo. Seguiremos
-          explorando nuevas formas de hacer moda de manera más responsable,
-          siempre manteniendo nuestra promesa de calidad y sofisticación.
-        </p>
+        <div className="about-text-container">
+          <p className="about-text">
+            Para nosotros, la sostenibilidad y la calidad son pilares
+            fundamentales en nuestro universo. Creemos en una moda consciente y
+            responsable, y aunque estamos en el inicio de este camino, cada paso
+            cuenta.
+          </p>
+          <p className="about-text">
+            En esta primera fase, hemos dado el primer paso con empaques
+            elaborados con materiales biodegradables, contribuyendo así a
+            reducir nuestro impacto ambiental. Este es solo el comienzo.
+            Seguiremos explorando nuevas formas de hacer moda de manera más
+            responsable, siempre manteniendo nuestra promesa de calidad y
+            sofisticación.
+          </p>
+        </div>
       </div>
 
-      <div className="about-content section-padding">
+      <div className="about-content">
         <div className="image-container">
-          <div className="about-img"></div>
-          <h2 className="about-title">DETALLES</h2>
+          <img
+            src={detailsImg}
+            className="about-img"
+            alt="Cociendo botón a tela"
+          ></img>
+          <h2 className="about-title white-text">DETALLES</h2>
         </div>
-        <p className="about-text">
-          Aquí es donde reside la verdadera belleza de MACAR: en los detalles.
-          Cada pieza está cuidadosamente elaborada, con terminaciones hechas a
-          mano que reflejan el amor y la dedicación que ponemos en cada diseño.
-          Creemos que el lujo está en la atención a los pequeños detalles, en la
-          calidad de los acabados y en la historia que cada prenda cuenta.
-        </p>
+        <div className="about-text-container">
+          <p className="about-text">
+            Aquí es donde reside la verdadera belleza de MACAR: en los detalles.
+            Cada pieza está cuidadosamente elaborada, con terminaciones hechas a
+            mano que reflejan el amor y la dedicación que ponemos en cada
+            diseño. Creemos que el lujo está en la atención a los pequeños
+            detalles, en la calidad de los acabados y en la historia que cada
+            prenda cuenta.
+          </p>
+        </div>
       </div>
     </div>
   );
