0d4e40d6063fb5ce31c936413e5f669057aa61f0
Edwin Peña <edwinsp095@gmail.com>
2025-11-27 19:58:26 -0500

    refactor: move images from public/ to src/

---
+++ src/data/products.ts
@@ -0,0 +1,89 @@
+import hashiDress1 from '../images/hashi-dress1.jpg';
+import hashiDress2 from '../images/hashi-dress2.jpg';
+import hashiDress3 from '../images/hashi-dress3.jpg';
+import hashiDress4 from '../images/hashi-dress4.jpg';
+
+import sunaBlouse1 from '../images/suna-blouse1.jpg';
+import sunaBlouse2 from '../images/suna-blouse2.jpg';
+import sunaBlouse3 from '../images/suna-blouse3.jpg';
+import sunaBlouse4 from '../images/suna-blouse4.jpg';
+import sunaBlouse5 from '../images/suna-blouse5.jpg';
+import sunaBlouse6 from '../images/suna-blouse6.jpg';
+
+import mossPant1 from '../images/moss-pant1.jpg';
+import mossPant2 from '../images/moss-pant2.jpg';
+import mossPant3 from '../images/moss-pant3.jpg';
+import mossPant4 from '../images/moss-pant4.jpg';
+import mossPant5 from '../images/moss-pant5.jpg';
+import mossPant6 from '../images/moss-pant6.jpg';
+
+import takiSet1 from '../images/taki-set1.jpg';
+import takiSet2 from '../images/taki-set2.jpg';
+import takiSet3 from '../images/taki-set3.jpg';
+import takiSet4 from '../images/taki-set4.jpg';
+import takiSet5 from '../images/taki-set5.jpg';
+
+import sabiBlouse1 from '../images/sabi-blouse1.jpg';
+import sabiBlouse2 from '../images/sabi-blouse2.jpg';
+import sabiBlouse3 from '../images/sabi-blouse3.jpg';
+import sabiBlouse4 from '../images/sabi-blouse4.jpg';
+import sabiBlouse5 from '../images/sabi-blouse5.jpg';
+import sabiBlouse6 from '../images/sabi-blouse6.jpg';
+
+export const products = [
+  {
+    id: 'p-1',
+    title: 'Hashi Dress',
+    description: 'Vestido largo con textura, entorchado en tirante y abertura en ambos lados.',
+    colors: {
+      black: '#000',
+    },
+    images: [hashiDress1, hashiDress2, hashiDress3, hashiDress4],
+    price: '340.000 COP',
+  },
+  {
+    id: 'p-2',
+    title: 'Suna Blouse',
+    description: 'Blusa drapeada con escote bandeja y piedra natural bordada a mano.',
+    colors: {
+      ivory: '#fffdfb',
+      black: '#000',
+    },
+    images: [sunaBlouse1, sunaBlouse2, sunaBlouse3, sunaBlouse4, sunaBlouse5, sunaBlouse6],
+    price: '260.000 COP',
+  },
+  {
+    id: 'p-3',
+    title: 'Moss Pant',
+    description: 'Pantalón tiro alto con abertura en bota, botones forrados y bolsillos traseros.',
+    colors: {
+      ivory: '#fffdfb',
+      black: '#000',
+    },
+    images: [mossPant1, mossPant2, mossPant3, mossPant4, mossPant5, mossPant6],
+    price: '290.000 COP',
+  },
+  {
+    id: 'p-4',
+    title: 'Taki Set',
+    description:
+      'Set de top largo con  manga campana y escote profundo redondo. Short tiro alto con bolsillo en costado, ambos con detalle de plisado.',
+    colors: {
+      brown: '#4A2922',
+      black: '#000',
+    },
+    images: [takiSet1, takiSet2, takiSet3, takiSet4, takiSet5],
+    price: '350.000 COP',
+  },
+  {
+    id: 'p-5',
+    title: 'Sabi Blouse',
+    description: 'Blusa de silueta geométrica y escote redondo, abertura en costados y silueteado con detalle en 3D.',
+    colors: {
+      brown: '#B0927A',
+      ivory: '#fffdfb',
+    },
+    images: [sabiBlouse1, sabiBlouse2, sabiBlouse3, sabiBlouse4, sabiBlouse5, sabiBlouse6],
+    price: '190.000 COP',
+  },
+];
